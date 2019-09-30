9df8fc5eea13a8cdf10756ce295148f78815cfd9
Brigham Campbell <me@brighamcampbell.com>
2019-09-29 23:57:45 -0600

    Passing all unit tests! Hellz yeah!

--- code/src/main/java/edu/isu/cs2235/Driver.java
+++ code/src/main/java/edu/isu/cs2235/Driver.java
@@ -1,16 +1,13 @@
 package edu.isu.cs2235;
 
-import edu.isu.cs2235.algorithms.impl.MergeSort;
-import edu.isu.cs2235.algorithms.impl.QuickSort;
-import edu.isu.cs2235.algorithms.impl.SelectionSort;
-import edu.isu.cs2235.algorithms.impl.TimSort;
+import edu.isu.cs2235.algorithms.impl.*;
 
 import java.util.Arrays;
 
 public class Driver {
     public static void main(String[] args) {
-        TimSort sort = new TimSort();
-        Integer[] array = {5, 4, 6, 2, 1};
+        HybridSort sort = new HybridSort();
+        Integer[] array = {5, 4, 6, 2, 1, 4, 2, 9, 8, 7, 1, 0, 4, 2, 7, 9};
         sort.sort(array);
 
         System.out.println(Arrays.toString(array));
--- code/src/main/java/edu/isu/cs2235/algorithms/impl/HybridSort.java
+++ code/src/main/java/edu/isu/cs2235/algorithms/impl/HybridSort.java
@@ -18,8 +18,57 @@ public class HybridSort implements ArraySort {
     }
 
     private <E extends Comparable> void hybridSort(E[] array, int left, int right) {
-        if(left < right) {
-            int pi = partition(array, left, right);
+        if(left >= right)
+            return;
+        if(right - left < 10) {
+            insertionSortRange(array, left, right);
+            return;
         }
+        int pi = partition(array, left, right);
+        hybridSort(array, left, pi);
+        hybridSort(array, pi + 1, right);
+    }
+
+    private <E extends Comparable> void insertionSortRange(E[] array, int start, int end) {
+        for(int i = start; i <= end; i++) {
+            insert(array, i, array[i], start);
+        }
+    }
+
+    private <E extends Comparable> void insert(E[] array, int pos, E value, int start) {
+        int i = pos - 1;
+        while(i >= start && array[i].compareTo(value) > 0) {
+            array[i + 1] = array[i];
+            i--;
+        }
+        array[i + 1] = value;
+    }
+
+    private <E extends Comparable> int partition(E[] array, int left, int right) {
+        E pivot = array[left];
+
+        int i = left + 1;
+        int j = right;
+        while(i <= j) {
+            if(array[i].compareTo(pivot) > 0 && array[j].compareTo(pivot) <= 0) {
+                swap(array, i, j);
+                i++;
+                j--;
+            } else {
+                if(array[i].compareTo(pivot) <= 0) {
+                    i++;
+                }
+                if(array[j].compareTo(pivot) > 0) {
+                    j--;
+                }
+            }
+        }
+        return j;
+    }
+
+    private <E> void swap(E[] array, int first, int second) {
+        E temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
     }
 }
